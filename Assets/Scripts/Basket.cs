5b749f85501b3936509d728bfbf7196012f7652d
BurningFnS <raqeefrashid01@gmail.com>
2022-12-28 16:02:15 +0800

    scripts added

---
+++ Assets/Scripts/Basket.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Basket : MonoBehaviour
+{
+    public GameManager gamemanager;
+
+    public Image Lose;
+
+    private List<string> ItemList = new List<string>();
+    private List<string> JunkList = new List<string>();
+    private List<string> BadItemsList = new List<string>();
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Food")
+        {
+            ItemList.Add(gameObject.name);
+            Debug.Log("ItemList Count: " + ItemList.Count);
+            Destroy(other.gameObject);
+
+            if (ItemList.Count > 15)
+            {
+                Debug.Log("You have severely overbought. Please KYS");
+                Lose.gameObject.SetActive(true);
+                gamemanager.endGame = true;
+            }
+        }
+
+        if (other.gameObject.tag == "Junk")
+        {
+            JunkList.Add(gameObject.name);
+            Debug.Log("JunkList Count: " + JunkList.Count);
+            Destroy(other.gameObject);
+
+            if (JunkList.Count >= 3)
+            {
+                Debug.Log("You Lost! Too much junk food");
+                Lose.gameObject.SetActive(true);
+                gamemanager.endGame = true;
+
+            }
+        }
+
+        if (other.gameObject.tag == "Inedible")
+        {
+
+            BadItemsList.Add(gameObject.name);
+            Debug.Log("BadItemsList Count: " + BadItemsList.Count);
+            Destroy(other.gameObject);
+
+            if (BadItemsList.Count >= 3)
+            {
+                Debug.Log("-1 point");
+
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    private Vector3 startingLine;
+    IEnumerator isGameRunning; //This variable can change name. It is necessary to "pause" the loop because if u directly pause it, it will start from initial.
+    [SerializeField]
+    private GameObject[] groceriesList;
+    [SerializeField]
+    private GameObject[] junkList;
+    [SerializeField]
+    private GameObject[] baditemList;
+
+    [HideInInspector]
+    public bool endGame = false;
+
+    public Button pauseButton;
+    public Button resumeButton;
+
+
+    void Awake()
+    {
+        startingLine = new Vector3(Random.Range(-2.2f, 2.2f), 5.7f, 0f);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isGameRunning = GameLoop();
+        StartCoroutine(isGameRunning);
+
+        Button pausebtn = pauseButton.GetComponent<Button>();
+        pausebtn.onClick.AddListener(pausing);
+
+        Button resumebtn = resumeButton.GetComponent<Button>();
+        resumebtn.onClick.AddListener(resume);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        startingLine = new Vector3(Random.Range(-2.2f, 2.2f), 5.7f, 0f);
+
+
+        /*------------------------------OLD PAUSE BUTTON----------------------------*/
+        //if (Input.GetKeyDown(KeyCode.O))
+        //{
+        //    Debug.Log("Game Loop Has been resumed");
+        //    StartCoroutine(isGameRunning);
+        //}
+
+        //if (Input.GetKeyDown(KeyCode.P))
+        //{
+        //    Debug.Log("Game Loop Has been paused");
+        //    StopCoroutine(isGameRunning);
+        //}   
+    }
+
+    IEnumerator GameLoop()
+    {
+        while (endGame == false)
+        {
+            Instantiate(groceriesList[Random.Range(0, groceriesList.Length)], startingLine, Quaternion.identity);
+            yield return new WaitForSeconds(1.7f); // DELAY FOR EACH INSTANTIATE 
+
+            Instantiate(junkList[Random.Range(0, junkList.Length)], startingLine, Quaternion.identity);
+            yield return new WaitForSeconds(2.3f); // DELAY FOR EACH INSTANTIATE 
+
+            Instantiate(baditemList[Random.Range(0, baditemList.Length)], startingLine, Quaternion.identity);
+            yield return new WaitForSeconds(2.3f); // DELAY FOR EACH INSTANTIATE 
+
+            Debug.Log("Game is running");
+            yield return new WaitForSeconds(1f);
+        }
+    }
+
+    void pausing()
+    {
+        Debug.Log("Game is paused!");
+        StopCoroutine(isGameRunning);
+    }
+    void resume()
+    {
+        Debug.Log("Game is resumed!");
+        StartCoroutine(isGameRunning);
+
+    }
+
+}
