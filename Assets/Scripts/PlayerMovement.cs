ae0bae870311d2541d7430c3f5b40f97aa32957b
iIIogic <shaynefang2003@gmail.com>
2023-01-03 00:47:27 +0800

    Added UI
    Added timer and fullness UI in the platformer

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerMovement : MonoBehaviour
-{    
+{
+    public TextMeshProUGUI TimingText, FullnessText;
+
     public Rigidbody2D rb;
 
+    [HideInInspector]
+    public int fullnessPercentage;
+    public float timeRemaining;
     public float moveSpeed = 7f;
     public float gravityPowerup;
 
@@ -18,6 +24,8 @@ public class PlayerMovement : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         gravityPowerup = rb.gravityScale;
+        timeRemaining = 60f;
+        fullnessPercentage = 0;
     }
 
     // Update is called once per frame
@@ -36,6 +44,8 @@ public class PlayerMovement : MonoBehaviour
             boostTimer = 3.0f;
             isTimerRunning = false;
         }
+        TimingText.text = "Time Remaining: " + Mathf.RoundToInt(timeRemaining -= Time.smoothDeltaTime);
+        FullnessText.text = "Fullness: " + fullnessPercentage;
     }
 
     private void FixedUpdate()
@@ -43,6 +53,7 @@ public class PlayerMovement : MonoBehaviour
         Vector2 velocity = rb.velocity;
         velocity.x = moveX;
         rb.velocity = velocity;
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -64,8 +75,8 @@ public class PlayerMovement : MonoBehaviour
 
         if(collision.gameObject.tag == "Food")
         {
+            fullnessPercentage += 5;
             collision.gameObject.SetActive(false);
-
         }
     }
 }
